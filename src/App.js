8f1a4409c260416b05138974376c45e1aeaa3084
devdong9897 <dev.dong9897@gmail.com>
2024-11-16 13:31:27 +0900

    Summary페이지 Form구현하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import SummaryPage from "./pages/SummaryPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SummaryPage />
+    </div>
+  );
 }
 
 export default App;
